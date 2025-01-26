314c53a35ab5a6b0e394fe5c2c5cc8525abc95eb
Alex Bruce <brucealmightyalex@yahoo.com>
2025-01-26 14:19:08 -0700

    Add basic summary and ingredients list to recipe page

--- src/RecipeDetails.js
+++ src/RecipeDetails.js
@@ -5,7 +5,7 @@ import "./RecipeDetails.css";
 const RecipeDetails = ({ recipes, dietIcons, fetchRecipeById }) => {
   const { id } = useParams();
   const recipe = recipes.find((recipe) => recipe.id === parseInt(id));
-
+  
   const [visibleRecipe, setVisibleRecipe] = useState({});
 
   useEffect(() => {
@@ -28,30 +28,53 @@ const RecipeDetails = ({ recipes, dietIcons, fetchRecipeById }) => {
   }, [recipes, id]);
   
   return (
-    <div className="recipe-details">
-      <img 
-        className="recipe-bg-image" 
-        src={visibleRecipe && visibleRecipe.image} 
-        alt={visibleRecipe && visibleRecipe.title} />
-      <div className="recipe-image-overlay"></div>
-      <div className="recipe-details-top-section">
-        <h1 className="recipe-details-title">{visibleRecipe.title && visibleRecipe.title.split(" ").map(word => {
-          return word.charAt(0).toUpperCase() + word.slice(1);
-        }).join(" ")}</h1>
-        <div className="recipe-details-tags">
-          {visibleRecipe && visibleRecipe.diets > 0 && visibleRecipe.diets.map((tag, index) => {
-            return (
-              <div key={index} className="recipe-details-tag">
-                <img 
-                  className="recipe-details-tag-icon" 
-                  alt={tag}
-                  src={dietIcons[tag]} />
-              </div>
-            )
-          })}
+    visibleRecipe ? (
+
+      <div className="recipe-details">
+        <img 
+          className="recipe-bg-image" 
+          src={visibleRecipe && visibleRecipe.image} 
+          alt={visibleRecipe && visibleRecipe.title} />
+        <div className="recipe-image-overlay"></div>
+        <div className="recipe-details-top-section">
+           <a
+            className="recipe-details-title"
+            href={visibleRecipe.sourceUrl}>
+              {visibleRecipe.title.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")}
+          </a>
+          <div className="recipe-details-tags">
+            {visibleRecipe && visibleRecipe.diets > 0 && visibleRecipe.diets.map((tag, index) => {
+              return (
+                <div key={index} className="recipe-details-tag">
+                  <img 
+                    className="recipe-details-tag-icon" 
+                    alt={tag}
+                    src={dietIcons[tag]} />
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        <div className="recipe-details-bottom-section">
+          <p className="recipe-details-summary">{visibleRecipe.summary}</p>
+          <div className="recipe-details-ingredients">
+             <h3>Ingredients</h3>
+             <ul>
+               {visibleRecipe.extendedIngredients.map((ingredient, index) => {
+                return (
+                  <li 
+                    key={index}>{ingredient.original}</li>
+                )
+              })}
+            </ul>
+           </div>
         </div>
       </div>
-    </div>
+    ) : (
+      <div className="recipe-details">
+        <h1 className="recipe-details-title">Recipe Not Found</h1>
+      </div>
+    )
   );
 };
 
